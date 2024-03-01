84886230bc35b8cc1da94c21ea57ddf09b6b59cc
kosisnk <145112781+kosisnk@users.noreply.github.com>
2024-03-01 20:41:42 +0100

    Add files via upload

---
+++ src/main/java/com/karolkosinski/App.java
@@ -0,0 +1,13 @@
+package com.karolkosinski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
